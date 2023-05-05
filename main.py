9c75f0b60ee6ee853c53bacfbd57bad74be6f66e
Kostya <kostyukovkg@yahoo.com>
2023-05-05 15:46:09 +0300

    Day 24: Add footers

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4") #p-portrait, mm-miillimeters
+pdf.set_auto_page_break(auto=False, margin=0) # turns off automatic page break
 
 pdf.add_page() # добавить новую страницу
 
@@ -19,12 +20,27 @@ df = pd.read_csv('topics.csv')
 pdf.set_font(family="Times", style="B", size=14)
 for index, page in df.iterrows():
     pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=18)
     pdf.set_text_color(0,0,254)
     pdf.cell(w=0, h=12, txt=page['Topic'], align='L', ln=1, border=0)
     pdf.line(x1=10, y1=20, x2=200, y2=20) # in mm as we defined before. x - dist from left, y - dist from up
+
+    pdf.ln(260) # adds break line
+
+    # set the footer
+    pdf.set_font(family="Times", style="I", size=8)  # new font style is set
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=page['Topic'], align='R')
+
     for i in range(page['Pages']-1):
         pdf.add_page()
-        pdf.line(x1=10, y1=20, x2=200, y2=20)
+        # pdf.line(x1=10, y1=20, x2=200, y2=20)
+        pdf.ln(272)  # adds break line
+
+        # set the footer
+        pdf.set_font(family="Times", style="I", size=8)  # new font style is set
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=page['Topic'], align='R')
 
 
 pdf.output("output.pdf")
